a48712602e7754bdb6ceea5a72b0a370a29b0b33
JessicaS161995 <ojitos16@outlook.es>
2023-10-23 15:24:44 -0500

    reflejo de la pelotita y creacion de la segunda

--- sketch.js
+++ sketch.js
@@ -11,15 +11,22 @@ function draw() {
   let posY = map(noise(noiseTime + 100), 0, 1, 0, windowHeight);
 
   let tam = map(noise(noiseTam), 0, 1, 50, 5);
-  noiseTime += 0.009;
+  noiseTime += 0.006;
   noiseTam += 0.1;
 
-  fill(255);
-  stroke(0, 50);
-  strokeWeight(3);
+  fill(15, 125, 15);
+  stroke(100, 50, 0, 50);
+  strokeWeight(5);
   circle(posX, posY, tam);
 
+  let posX2 = posX;
+  let posY2 = windowHeight - posY;
+  let tam2 = tam;
 
-  
+  fill(150, 15, 90);
+  stroke(25, 110, 18, 50);
+  strokeWeight(5);
+  circle(posX2, posY2, tam2);
+
 
 }

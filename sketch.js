5395fdd89a39ae433d1cf8647564653ef6295954
JessicaS161995 <ojitos16@outlook.es>
2023-10-23 14:55:47 -0500

    cambio a transparencia del map con el circulo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let noiseTime = 0
+let noiseTam = 10;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  //background(255);
+  let posX = map(noise(noiseTime), 0, 1, 0, windowWidth);
+  let posY = map(noise(noiseTime + 100), 0, 1, 0, windowHeight);
+
+  let tam = map(noise(noiseTam), 0, 1, 50, 5);
+  noiseTime += 0.009;
+  noiseTam += 0.1;
+
+  fill(255);
+  stroke(0, 50);
+  strokeWeight(3);
+  circle(posX, posY, tam);
+
+
+  
+
 }

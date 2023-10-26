ec1b6e333f07f8d5a7fb706d21d908634aef15ed
JessicaS161995 <ojitos16@outlook.es>
2023-10-26 00:25:55 -0500

    los circulos llegan hasta el ancho de la pantalla y regresan

--- sketch.js
+++ sketch.js
@@ -1,32 +1,56 @@
-let noiseTime = 0
+let noiseTime = 0;
 let noiseTam = 20;
+let posX = 0;
+let posY = 0;
+let velX = 2;
+let velY = 2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-  //background(255);
-  let posX = map(noise(noiseTime), 0, 1, 0, windowWidth);
-  let posY = map(noise(noiseTime + 100), 0, 1, 0, windowHeight);
+//background(255);
+  
+  posX += velX;
+  posY += velY;
 
-  let tam = map(noise(noiseTam), 0, 1, 50, 5);
+  if (posX < 0 || posX > windowWidth) {
+    velX *= -1;
+    posX = constrain(posX, 0, windowWidth);
+  }
+
+  if (posY < 0 || posY > windowHeight) {
+    velY *= -1;
+    posY = constrain(posY, 0, windowHeight);
+  }
+
+  let tam = map(noise(noiseTam), 0, 1, 50, 10);
   noiseTime += 0.006;
   noiseTam += 0.01;
 
-  fill(15, 125, 15);
-  stroke(100, 50, 0, 50);
-  strokeWeight(5);
+  fill(10, 50, 50);
+  stroke(2, 165, 73, 120);
+  strokeWeight(2);
   circle(posX, posY, tam);
 
-  let posX2 = posX;
+  let posX2 = windowWidth - posX;
   let posY2 = windowHeight - posY;
   let tam2 = tam;
 
-  fill(150, 15, 90);
-  stroke(100, 50, 0, 50);
-  strokeWeight(5);
+  fill(21, 17, 150);
+  stroke(126, 126, 78, 90);
+  strokeWeight(2);
   circle(posX2, posY2, tam2);
 
+  // Cuadrado 2
+  let posX4 = map(noise(noiseTime + 400), 0, 1, 0, windowWidth);
+  let posY4 = map(noise(noiseTime + 100), 0, 1, 0, windowHeight);
+  let tam4 = map(noise(noiseTam + 100), 0, 1, 30, 5);
 
+  fill(255, 150); // Agregar transparencia
+  stroke(70, 125, 35, 50);
+  strokeWeight(5);
+  rect(posX4, posY4, tam4, tam4);
 }
